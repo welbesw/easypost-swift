066a2567890d099574c59a80a144f21cbca8dc61
William Welbes <williamj@gmail.com>
2015-10-26 17:32:53 -0500

    Removed unused Quick and Nimble pods that were causing issues in Xcode 7.1

--- Example/Pods/Target Support Files/Pods-EasyPostApi_Tests/Pods-EasyPostApi_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_EasyPostApi_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_EasyPostApi_Tests
-@end
--- Example/Pods/Target Support Files/Pods-EasyPostApi_Tests/Pods-EasyPostApi_Tests-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_EasyPostApi_TestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_EasyPostApi_TestsVersionString[];
-
